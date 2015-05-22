4595aa4c2830e5c270cc7e30c5102f62344b1fa0
Michiel Dethmers <git@phplist.com>
2015-05-22 16:55:53 +0100

    update terminology

--- en/messages.php
+++ en/messages.php
@@ -1,5 +1,5 @@
-You can use "Requeue" to resend a message. This will cause the message to be sent to users who have subscribed after you sent the message. It will not be sent to users who have already received the message.
-<p>If you view a message, you will be able to resend it to a different list</p>
+You can use "Requeue" to resend a campaign. This will cause the campaign to be sent to subscribers who sign up later. It will not be sent to subscribers who have already received the campaign.
+<p>If you view a campaign, you will be able to resend it to a different list</p>
 <?php if (TEST) { ?>
-<br /><b>Note:</b> You are operating in test mode, so messages will be "resend" to users who have received it.
+<br /><b>Note:</b> You are operating in test mode, so campaigns will be "resend" to subscribers who have received it.
 <?php } ?>
--- en/reconcileusers.php
+++ en/reconcileusers.php
@@ -1,1 +1,1 @@
-<p><h3>This page allows you to clean up the database of users</h3></p>
+<p><h3>This page allows you to clean up the database of subscribers</h3></p>
